1ecd04006f8a9be15d8c1e3c31b744e4c1fe4563
Adjei Rapheal <ralphrapheals@gmail.com>
2024-02-14 02:02:38 +0000

    First Push

---
+++ arrays.js
@@ -0,0 +1,22 @@
+//Arrays 
+const person = ["ralph", "adjei" , 89]
+
+console.log()
+const studentDetails = {
+    name: "Carl ",
+    level: 300,
+    school: "Saint thomas Aquina"
+}
+
+//conditional rendering
+const age = "18";
+
+//Build BMI Calculator
+//if says if condition is met perform the following instruction
+if (age === 18){
+    console.log("You are 18")
+}//Else is the default case 
+else {
+    console.log("You are not 18")
+}
+
