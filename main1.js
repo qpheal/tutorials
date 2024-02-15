233313c3055d8ba8fb50d27f97c6e7b96235f2a2
Adjei Rapheal <ralphrapheals@gmail.com>
2024-02-15 00:27:59 +0000

    Obects

---
+++ main1.js
@@ -0,0 +1,25 @@
+const studentDetails = {
+    name: "carl",
+    age: 34,
+    school: "Saint "
+
+}
+
+// string Methods
+const nameOfPerson = "Adjei Rapheal";
+
+const saying = "Lave is a beautiful thing "
+
+// length
+console.log("Length :", nameOfPerson.length)
+
+//includes
+console.log("Include : ", saying.includes("Love"))
+
+//Uppercase
+console.log (name.toUpperCase())
+
+//Number 
+let x = Number.MAX_VALUE
+
+
